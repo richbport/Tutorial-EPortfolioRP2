4fe9681ac7ad55fe6adec1712fc188a82736689e
Rich <richbport@yahoo.com>
2025-07-09 12:57:08 -0400

    removing loading state after timeout

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ function contact(event) {
   //     .then(() => {
   //       console.log("this worked1");
   //     });
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
   setTimeout(() => {
+    loading.classList.remove("modal__overlay--visible")
     console.log("it worked 2");
   }, 500);
 }
