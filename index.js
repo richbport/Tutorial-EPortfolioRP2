a74779cb5c099cc03e38922367ac050e4b1e4e46
Rich <richbport@yahoo.com>
2025-07-10 17:36:59 -0400

    throw an error

--- index.js
+++ index.js
@@ -15,14 +15,15 @@ function contact(event) {
       "-a810SH9YGkTxZ96D"
     )
     .then(() => {
+      throw new Error("error");
       loading.classList.remove("modal__overlay--visible");
       success.classList += " modal__overlay--visible";
     })
     .catch(() => {
       loading.classList.remove("modal__overlay--visible");
       alert(
-        "The email service is temporarily unavailable."
-      )
+        "The email service is temporarily unavailable. Please contact me directly at richbport@gmail.com."
+      );
     });
 
   loading.classList += " modal__overlay--visible";
